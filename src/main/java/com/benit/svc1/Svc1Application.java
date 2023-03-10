6af73f6944c6c213fe3df04a373f82853a06615e
이동진 <2dong0711@hanmail.net>
2023-03-10 12:19:35 +0900

    주석 추가

--- src/main/java/com/benit/svc1/Svc1Application.java
+++ src/main/java/com/benit/svc1/Svc1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Svc1Application {
 
 	public static void main(String[] args) {
+		// First Service 실행
 		SpringApplication.run(Svc1Application.class, args);
 	}
 
